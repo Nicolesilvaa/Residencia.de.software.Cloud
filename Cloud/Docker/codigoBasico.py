443d5af47e94ee8366beeb5d0952cfd6e88ff268
Nicole Silva <nicolesilva8144@gmail.com>
2024-09-07 15:48:04 -0300

    feat: atribuindo novas funcionalidades ao código

---
+++ Cloud/Docker/codigoBasico.py
@@ -0,0 +1,10 @@
+#Verifica se os valores do intervalo de 1 -50 são pares ou ímpares
+
+for i  in range(50):
+
+    if(i % 2 == 0):
+        print( i , " é par")
+    else:
+        print( i , "é ímpar")
+    
+print("Programa finalizado.")
